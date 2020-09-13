24f70b9946227a3a5150e60410861c6eb4019e06
soroosh-gb <soroosh.gb@gmail.com>
2020-09-12 23:24:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+
+function countDown(integer) {
+    let counter = 0
+    while (counter <= integer ) {
+        console.log(integer - counter);
+        counter++;
+    }
+}
